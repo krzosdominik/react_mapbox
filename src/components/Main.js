6e6bcbf3c21d457ec34cef68817d623e1473ae41
krzosdominik <krzosdominik@gmail.com>
2020-04-05 00:11:18 +0200

    add Main component

---
+++ src/components/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Container } from 'reactstrap';
+
+const Main = () => {
+    return (
+        <section className='section' style={{minHeight: '100vh'}}>
+            <Container>
+            
+            </Container>
+        </section>
+    )
+}
+
+export default Main;
