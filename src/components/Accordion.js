664a9d176d0848a5d02c4c5a220a3f74f9ccfc1e
krzosdominik <krzosdominik@gmail.com>
2020-04-05 00:59:46 +0200

    add Accordion and Map components

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
-import { Card, CardHeader, CardBody, Collapse, Button } from "reactstrap";
+import React, { useState } from 'react';
+import { Card, CardHeader, CardBody, Collapse, Button } from 'reactstrap';
 
 import { accordionAttributes as attributes } from '../settings/constants';
-
+import Map from './Map';
 
 const Accordion = () => {
     const [collapses, setCollapses] = useState([]);
@@ -12,39 +12,39 @@ const Accordion = () => {
         : setCollapses([...collapses, id]);
 
     return (
-        <div id="acordeon">
-            <div aria-multiselectable={true} id="accordion" role="tablist">
-                <Card className="no-transition">
+        <div id='acordeon'>
+            <div aria-multiselectable={true} id='accordion' role='tablist'>
+                <Card className='no-transition'>
 
-                    <CardHeader className="card-collapse" id="headingOne" role="tab">
+                    <CardHeader className='card-collapse' id='headingOne' role='tab'>
                         <Button
-                            aria-expanded={collapses.includes("collapseOne")}
-                            id="collapseOne"
+                            aria-expanded={collapses.includes('collapseOne')}
+                            id='collapseOne'
                             onClick={ ({ target }) => changeCollapse(target.id)}
                             {...attributes}
                         >
-                            <i className="now-ui-icons location_map-big"></i>{" "}
+                            <i className='now-ui-icons location_map-big'></i>{' '}
                             Map
                         </Button>
                     </CardHeader>
-                    <Collapse isOpen={collapses.includes("collapseOne")}>
+                    <Collapse isOpen={collapses.includes('collapseOne')}>
                         <CardBody>
-                
+                            <Map />
                         </CardBody>
                     </Collapse>
 
-                    <CardHeader className="card-collapse" id="headingTwo" role="tab">
+                    <CardHeader className='card-collapse' id='headingTwo' role='tab'>
                         <Button
-                            aria-expanded={collapses.includes("collapseTwo")}
-                            id="collapseTwo"
+                            aria-expanded={collapses.includes('collapseTwo')}
+                            id='collapseTwo'
                             onClick={ ({ target }) => changeCollapse(target.id)}
                             {...attributes}
                         >
-                            <i className="now-ui-icons location_pin"></i>{" "}
+                            <i className='now-ui-icons location_pin'></i>{' '}
                             Markers information
                         </Button>
                     </CardHeader>
-                    <Collapse isOpen={collapses.includes("collapseTwo")}>
+                    <Collapse isOpen={collapses.includes('collapseTwo')}>
                         <CardBody>
                             
                         </CardBody>
--- src/settings/constants.js
+++ src/settings/constants.js
@@ -1,13 +1,15 @@
+export const MAPBOX_TOKEN = 'pk.eyJ1Ijoia3J6b3Nkb21pbmlrIiwiYSI6ImNrOGtxdjMybzAzcmgzbW8xOWt2bzF4aHEifQ.RBMOKHxbivXQCXNTm--rHA';
+
 const accordionButtonStyle = {
     width: '100%',
     position: 'relative'
 };
 
 export const accordionAttributes = {
-    color: "info",
-    className: "collapsed panel-title h5 my-1 text-left",
-    type: "button",
-    "data-parent": "#accordion",
-    size: "sm",
+    color: 'info',
+    className: 'collapsed panel-title h5 my-1 text-left',
+    type: 'button',
+    'data-parent': '#accordion',
+    size: 'sm',
     style: accordionButtonStyle
 };
