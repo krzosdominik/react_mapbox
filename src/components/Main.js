0e58eb0fb8f29f4dc3b537ab416722c638fe0f6d
krzosdominik <krzosdominik@gmail.com>
2020-04-05 00:38:37 +0200

    add Accordion component

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { Container } from 'reactstrap';
 
+import Accordion from './Accordion';
+
 const Main = () => {
     return (
-        <section className='section' style={{minHeight: '100vh'}}>
+        <section className='section content-center' style={{ minHeight: '100vh' }}>
             <Container>
-            
+                <h2 className='text-center'>
+                    Test app and <strong className="text-info">have fun!</strong>
+                </h2>
+                <Accordion />
             </Container>
         </section>
     )
