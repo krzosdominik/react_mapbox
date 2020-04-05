c471fa2d605df7f8226934c238480c048aea6ee8
krzosdominik <krzosdominik@gmail.com>
2020-04-05 19:47:44 +0200

    add table pagination

--- src/components/MarkersInfo.js
+++ src/components/MarkersInfo.js
@@ -2,22 +2,40 @@ import React, { useState } from 'react';
 
 import { useMarkers } from '../context/Markers.context';
 import InfoTable from './InfoTable';
+import Pagination from './Pagination';
 import InfoMessage from './InfoMessage';
 
 const MarkersInfo = () => {
     const { markers } = useMarkers();
     const [isMessageVisible, setMessageVisible] = useState(true);
+    const [currentPage, setCurrentPage] = useState(0);
+
+    const pageSize = 10;
+    const pagesCount = Math.ceil(markers.length / pageSize);
 
     const handleAlertClose = () => setMessageVisible(false);
+    const handlePaginationClick = index => setCurrentPage(index);
 
     return (
         <>
-            {markers.length !== 0 && <InfoTable />}
             {markers.length === 0 && isMessageVisible && (
                 <InfoMessage
                     onClose={handleAlertClose}
                     visible={isMessageVisible}
                 />
+            )}
+            {markers.length !== 0 && (
+                <>
+                    <InfoTable
+                        currentPage={currentPage}
+                        pageSize={pageSize}
+                    />
+                    <Pagination
+                        currentPage={currentPage}
+                        pagesCount={pagesCount}
+                        onClick={handlePaginationClick}
+                    />
+                </>
             )}
         </>
     )
